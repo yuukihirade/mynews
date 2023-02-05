fe715531b10016559b147d27e021c416514c9513
y-hirade <yuuki.h.0909@gmail.com>
2023-02-05 09:26:11 +0000

    [Laravel]04

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+use App\Http\Controllers\Admin\AAAController;
+Route::controller(AAAController::class)->prefix('admin')->group(function(){
+    Route::get('/XXX','bbb');
+});
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function(){
+    Route::get('profile/create','add');
+    Route::get('profile/edit','edit');
+});

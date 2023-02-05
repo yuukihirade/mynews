9720a75cd66b388c199efa2edea27f8fea2de55b
y-hirade <yuuki.h.0909@gmail.com>
2023-02-05 06:17:06 +0000

    First commit

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    //以下を追記した
+    public function add()
+    {
+        return view('admin.news.create');
+    }
+}

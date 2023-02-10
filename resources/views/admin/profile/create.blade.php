166845f6f8f32711128cfd24d6786f05c8e12383
y-hirade <yuuki.h.0909@gmail.com>
2023-02-10 11:37:18 +0000

    [PHP/Laravel]06 課題

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+@section('title', 'プロフィールの新規作成')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
